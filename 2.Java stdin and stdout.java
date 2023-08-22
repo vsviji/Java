59cf6e03cc0f66777dc41489efd7b7389de71e3d
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-08-22 19:50:35 +0530

    Day-2

---
+++ 2.Java stdin and stdout.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //give input to the integers stdin
+        //to take string as input String myString = scanner.next();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        
+        scan.close();
+        
+        //print the integers stdout
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
