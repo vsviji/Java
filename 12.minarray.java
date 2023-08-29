f0b73117aef47e3f3ab5d0844a6b6ed015b7c701
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-08-29 20:54:07 +0530

    Day-9

---
+++ 12.minarray.java
@@ -0,0 +1,15 @@
+class array{  
+
+static void min(int arr[]){  
+int min=arr[0];  
+for(int i=1;i<arr.length;i++)  
+ if(min>arr[i])  
+  min=arr[i];  
+  
+System.out.println("minimum number is "+min);  
+}  
+  
+public static void main(String args[]){  
+int a[]={33,3,4,5};//declaring and initializing an array  
+min(a);//passing array to method  
+}
