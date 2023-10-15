8d251422639c8c6b96f36041ecd016a7c62d00d0
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-10-15 12:16:13 +0530

    Create 19.StringPlaindrome.java

---
+++ 19.StringPlaindrome.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+        
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+    
+    // Function to check if a string is a palindrome
+    public static boolean isPalindrome(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase(); // Remove spaces and convert to lowercase
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
