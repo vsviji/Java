691b615e20828a42b11e61a10d2e55f8b1b73b63
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-10-18 18:35:23 +0530

    Create 21.Intergerpalindrome.java

---
+++ 21.Intergerpalindrome.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int input = scanner.nextInt();
+        scanner.close();
+        
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+    
+    // Function to check if a string is a palindrome
+    public static boolean isPalindrome(int n) {
+        String str = Integer.toString(n); // Remove spaces and convert to lowercase
+        int left = 0;
+        int right = str.length() - 1;
+        
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
