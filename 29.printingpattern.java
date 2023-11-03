e6e65aad11c32192320c3be5af9b4d114a854f60
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-11-03 22:48:21 +0530

    Day-28

---
+++ 29.printingpattern.java
@@ -0,0 +1,31 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 3;
+
+        // Upper part of the pattern
+        for (int i = 1; i <= n; i++) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+
+        // Lower part of the pattern
+        for (int i = n - 1; i >= 1; i--) {
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(); // Move to the next line
+        }
+    }
+}
